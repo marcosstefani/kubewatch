1085aaac628f0e8da2341a9daefa8462bf234d33
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2020-05-15 10:39:06 -0300

    use controllers into main.py

--- kubewatch/main.py
+++ kubewatch/main.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from service.kubernetes import KubernetesService
+from controller.pod import PodController
 from controller.service import ServiceController
+from controller.persistent_volume import PersistentVolumeController
+from controller.persistent_volume_claim import PersistentVolumeClaimController
+from controller.replica_set import ReplicaSetController
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +16,11 @@ class Kubernetes(Resource):
         return KubernetesService.all()
 
 api.add_resource(Kubernetes, '/')
+api.add_resource(PodController, '/pods/')
 api.add_resource(ServiceController, '/services/')
+api.add_resource(PersistentVolumeController, '/persistent_volumes/')
+api.add_resource(PersistentVolumeClaimController, '/persistent_volume_claims/')
+api.add_resource(ReplicaSetController, '/replica_sets/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
